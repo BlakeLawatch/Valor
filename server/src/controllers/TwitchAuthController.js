f1bd5e966e025db663afbe43f95820a18b25f8ec
Daniel Palomarez <daniel.palomarez5@gmail.com>
2023-11-08 15:56:35 -0700

    twitch service

--- server/src/controllers/TwitchAuthController.js
+++ server/src/controllers/TwitchAuthController.js
@@ -1,9 +1,19 @@
 import BaseController from "../utils/BaseController.js";
-
+import { twitchAuthService } from "../services/twitchAuthService.js";
 
 export class TwitchAuthController extends BaseController {
     constructor() {
-        super()
+        super('api/twitch')
+        this.router
+            .get('', this.getTwitchAuthToken)
+    }
 
+    async getTwitchAuthToken(req, res, next) {
+        try {
+            const token = await twitchAuthService.getTwitchAuthToken()
+            res.send(token)
+        } catch (error) {
+            next(error)
+        }
     }
 }
---
+++ server/src/services/twitchAuthService.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+
+class TwitchAuthService {
+
+    async getTwitchAuthToken() {
+        const env = {
+            clientSecret: process.env.client_secret,
+            clientId: process.env.client_id
+        }
+        const res = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${env.clientId}&client_secret=${env.clientSecret}&grant_type=client_credentials`)
+
+        return res
+    }
+}
+
+export const twitchAuthService = new TwitchAuthService
