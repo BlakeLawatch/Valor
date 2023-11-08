d1667f4fca1ce2496612a8c09b9cbce38a5e6f15
alaynadelauro <andelauro@yahoo.com>
2023-11-08 14:48:52 -0700

    account schema updated

--- server/src/models/Account.js
+++ server/src/models/Account.js
@@ -4,9 +4,18 @@ const Schema = mongoose.Schema
 export const AccountSchema = new Schema(
   {
     subs: [{ type: String, unique: true }],
-    email: { type: String, lowercase: true, unique: true },
+    email: { type: String, lowercase: true, unique: true, maxLength: 100 },
     name: { type: String, required: true },
-    picture: { type: String }
+    bio: { type: String, maxLength: 1000 },
+    picture: { type: String, maxLength: 500 },
+    coverImg: { type: String, maxLength: 500 },
+    twitchUrl: { type: String, maxLength: 500 },
+    youtubeUrl: { type: String, maxLength: 500 },
+    twitterUrl: { type: String, maxLength: 500 },
+    facebookUrl: { type: String, maxLength: 500 },
+    instagramUrl: { type: String, maxLength: 500 },
+    websiteUrl: { type: String, maxLength: 500 },
+
     // NOTE If you wish to add additional properties do so here
   },
   { timestamps: true, toJSON: { virtuals: true } }
