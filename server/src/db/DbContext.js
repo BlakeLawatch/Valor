ecd15a845be8441207c9a7784258adf21e2f6508
alaynadelauro <andelauro@yahoo.com>
2023-11-08 16:27:24 -0700

    matches have a schema

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -3,12 +3,14 @@ import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { TournamentSchema } from '../models/Tournament.js';
 import { PlayerSchema } from "../models/Player.js";
+import { matchSchema } from '../models/Match.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Tournaments = mongoose.model('Tournament', TournamentSchema);
-  Players = mongoose.model('Player', PlayerSchema)
+  Players = mongoose.model('Player', PlayerSchema);
+  Matches = mongoose.model('Match', matchSchema);
 }
 
 export const dbContext = new DbContext()
---
+++ server/src/models/Match.js
@@ -0,0 +1,30 @@
+import { Schema } from "mongoose";
+
+export const matchSchema = new Schema(
+    {
+        player1Id: { type: Schema.Types.ObjectId },
+        player2Id: { type: Schema.Types.ObjectId },
+        roundNumber: { type: Number },
+        tournamentId: { type: Schema.Types.ObjectId, required: true },
+        winnerId: { type: Schema.Types.ObjectId },
+    },
+    {
+        timestamps: true, toJSON: { virtuals: true }
+    }
+)
+matchSchema.virtual('player 1', {
+    localField: "player1Id",
+    foreignField: "_id",
+    ref: "Player",
+})
+matchSchema.virtual("player 2", {
+    localField: "player2Id",
+    foreignField: "_id",
+    ref: "Player",
+})
+matchSchema.virtual("tournament", {
+    localField: "tournamentId",
+    foreignField: "_id",
+    ref: "Tournament",
+    justOne: true
+})
