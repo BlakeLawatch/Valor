30ac9b8af62e6fa5e2c177293a96f46f8052092b
Matthew Wong <mattwong991@gmail.com>
2023-11-08 14:55:31 -0700

    Create a Player

--- server/src/services/PlayersService.js
+++ server/src/services/PlayersService.js
@@ -3,7 +3,10 @@ import { dbContext } from "../db/DbContext.js"
 class PlayersService {
 
   async createPlayer(playerData) {
-    // const player = await dbContext
+    const player = await dbContext.Players.create(playerData)
+    await player.populate('profile', 'name picture')
+    await player.populate('tournament', 'name gameName')
+    return player
   }
 
 }
