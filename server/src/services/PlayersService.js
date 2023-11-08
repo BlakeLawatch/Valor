1833cf48435793808b19b17399535edfca4ebbcb
Matthew Wong <mattwong991@gmail.com>
2023-11-08 14:01:27 -0700

    Init PlayerController, PlayerService, PlayerSchema

---
+++ server/src/services/PlayersService.js
@@ -0,0 +1,11 @@
+import { dbContext } from "../db/DbContext.js"
+
+class PlayersService {
+
+  async createPlayer(playerData) {
+    // const player = await dbContext
+  }
+
+}
+
+export const playersService = new PlayersService()
