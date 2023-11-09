672e152ad6c4df4a9bd1d73f2a68f84615b936dc
Blake Lawatch <blakelawatch@gmail.com>
2023-11-09 11:19:37 -0700

    Create modal mostly done

---
+++ client/src/components/createTournamentModal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="modal fade" id="createTournamentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Tournament Name</label>
+                <input type="text" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="gameName" class="form-label">Game Being Played</label>
+                <input type="text" class="form-control" id="gameName">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea name="description" id="" cols="55" rows="8"></textarea>
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<script>
+
+export default {
+    setup(){
+    return {  }
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
