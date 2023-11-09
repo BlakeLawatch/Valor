9505e1616b133f348f226286e896ce4836433de4
Daniel Palomarez <daniel.palomarez5@gmail.com>
2023-11-09 11:50:20 -0700

    backend api works

--- server/src/controllers/TwitchAuthController.js
+++ server/src/controllers/TwitchAuthController.js
@@ -9,12 +9,23 @@ export class TwitchAuthController extends BaseController {
         super('api/twitch')
         this.router
             .get('', this.getTwitchAuthToken)
+            .post('', this.getExternalApiResponse)
     }
 
     async getTwitchAuthToken(req, res, next) {
         try {
             const token = await twitchAuthService.getTwitchAuthToken()
             res.send(token)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getExternalApiResponse(req, res, next) {
+        try {
+            const body = req.body.search
+            const games = await twitchAuthService.getExternalApiResponse(body)
+            res.send(games)
         } catch (error) {
             next(error)
         }
--- server/src/services/twitchAuthService.js
+++ server/src/services/twitchAuthService.js
@@ -1,25 +1,52 @@
 import axios from 'axios/dist/node/axios.cjs'
+import { BadRequest } from '../utils/Errors.js'
 
+const env = {
+    clientSecret: process.env.client_secret,
+    clientId: process.env.client_id
+}
 
 class TwitchAuthService {
-
     constructor() {
+        this.token = ''
     }
     tokenData = {}
+    async getExternalApiResponse(body) {
+        let time = new Date().getTime()
+        if (!this.token || this.tokenData.expireTime - time >= 600000) {
+            await this.getTwitchAuthToken()
+        }
+        if (!body) {
+            throw new BadRequest("need something to search")
+        }
+
+        const res = await axios.request('https://api.igdb.com/v4/games', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Client-ID': env.clientId,
+                'Authorization': `Bearer ${this.token}`
+            },
+            data: `search "${body}"; 
+fields name, cover.url; 
+limit 10;`,
+        })
+        return res.data
+    }
+
+
     async getTwitchAuthToken() {
         let time = new Date().getTime()
         if (this.tokenData.expireTime - time >= 600000) {
-            return this.tokenData
+            return 'use old token'
         }
 
-        const env = {
-            clientSecret: process.env.client_secret,
-            clientId: process.env.client_id
-        }
         const res = await axios.request(`https://id.twitch.tv/oauth2/token?client_id=${env.clientId}&client_secret=${env.clientSecret}&grant_type=client_credentials`, { method: 'POST' })
         res.data.expireTime = time + res.data.expires_in
         this.tokenData = res.data
-        return this.tokenData
+        // TODO set this.token
+        this.token = res.data.access_token
+        return 'token made'
     }
 }
 
