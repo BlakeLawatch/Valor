90f249faf6966d7d8befbffbde1dd33ac3b476cd
Daniel Palomarez <daniel.palomarez5@gmail.com>
2023-11-09 10:31:59 -0700

    some front end stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,6 +14,7 @@ import Navbar from './components/Navbar.vue'
 
 export default {
   setup() {
+    // on mounted get twitch api key and attach it to twitch axios instance maybe not
     return {
       appState: computed(() => AppState)
     }
---
+++ client/src/services/GamesService.js
@@ -0,0 +1,12 @@
+import { logger } from "../utils/Logger"
+import { api, externalApi } from "./AxiosService"
+
+class GamesService{
+
+async homeSearch(value){
+
+}
+
+}
+
+export const gamesService = new GamesService()
