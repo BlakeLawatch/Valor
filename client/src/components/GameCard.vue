fdc2dc3dc9c05c37bd170ff64887cd26c53069f1
Daniel Palomarez <daniel.palomarez5@gmail.com>
2023-11-09 14:14:38 -0700

    appstate/game model

---
+++ client/src/components/GameCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>
+        games
+    </p>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+export default {
+    setup(){
+    return {  }
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/src/models/Game.js
@@ -0,0 +1,7 @@
+export class Game{
+    constructor(data){
+        this.id = data.id
+        this.cover = data.cover
+this.name = data.name
+    }
+}
