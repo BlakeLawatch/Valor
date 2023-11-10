6c5d2e3022488e8ffb8c3f83f9a1c09bc7a4b1e9
alaynadelauro <andelauro@yahoo.com>
2023-11-10 09:55:41 -0700

    more changes to account page

--- client/src/components/TournamentsCreatedByMe.vue
+++ client/src/components/TournamentsCreatedByMe.vue
@@ -1,6 +1,15 @@
 <template>
-<div v-for="tournament in mytournaments" :key="tournament.id" class="col-3 m-3 account-info-card">
-    <p class="fs-5 text-center text-light">{{ tournament.name }}</p>
+<div v-for="tournament in mytournaments" :key="tournament.id" class="col-10 col-sm-5 col-md-4 col-lg-3 m-3 account-info-card px-0">
+    <img v-if="tournament.imgUrl" :scr="tournament.imgUrl" class="tournament-image w-100 h-50"/>
+    <img v-else-if="!tournament.imgUrl && tournament.gameImg" :src="tournament.gameImg" class="tournament-image w-100 h-50"/>
+    <img v-else-if="!tournament.imgUrl && !tournament.gameImg" src="https://plus.unsplash.com/premium_photo-1695717076798-404f76f121da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGFuZGF8ZW58MHx8MHx8fDA%3D" class="tournament-image w-100 h-50"/>
+    <div class="h-50 d-flex flex-column justify-content-between">
+        <p class="fs-5 ps-2 text-light">{{ tournament.name }}</p>
+    <div class="w-100 d-flex justify-content-end pe-2">
+        <button class="btn color-match text-light"> Edit</button>
+    </div>
+    </div>
+    
 </div>
 </template>
 
@@ -38,6 +47,23 @@ export default {
 .account-info-card{
 background-color: rgb(68, 68, 68);
 box-shadow: 0px 5px 6px black;
-height: 10vh;
+height: 19vh;
+}
+.tournament-image{
+    object-fit: cover;
+    object-position: center;
+}
+.color-match{
+background-color: #2ca58d;
+}
+@media(max-width:1400px){
+.account-info-card{
+    height:25vh;
+}
+}
+@media(max-width:824px){
+    .account-info-card{
+        height: 30vh;
+    }
 }
 </style>
