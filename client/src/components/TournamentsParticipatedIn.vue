b6fdaac45bfeb7806f0322d4f4212db5b2c68c5a
alaynadelauro <andelauro@yahoo.com>
2023-11-13 11:50:48 -0700

    final styling changes(for now)

--- client/src/components/TournamentsParticipatedIn.vue
+++ client/src/components/TournamentsParticipatedIn.vue
@@ -18,8 +18,8 @@
     <div class="row w-100">
         <div v-for="player in participatedIn" :key="player.id" class="col-10 col-sm-5 col-md-4 col-lg-3 m-3 account-info-card px-0">
             <img v-if="player.tournament.imgUrl" :src="player.tournament.imgUrl" class="w-100 h-75 tournament-image">
-            <img v-else-if="!player.tournament.imgUrl && player.tournament.gameImg" :src="player.tournament.gameImg" class="w-100 h-75 tournament-image">
-            <img v-else-if="!player.tournament.imgUrl && !player.tournament.gameImg" src="src/assets/img/valorPanda.png" class="w-100 h-75 tournament-image">
+            <img v-else-if="!player.tournament.imgUrl && player.tournament.gameImg" :src="player.tournament.gameImg" class="w-100 tournament-image">
+            <img v-else-if="!player.tournament.imgUrl && !player.tournament.gameImg" src="src/assets/img/valorPanda.png" class="w-100 tournament-image">
             <div class="d-flex flex-column justify-content-between">
                 <p class="fs-5 ps-2 text-light">{{ player.tournament.name }}</p>            </div>
         </div>
@@ -70,15 +70,17 @@ background-color: #2ca58d;
 .tournament-image{
     object-fit: cover;
     object-position: center;
+    height: 12rem;
 }
 @media(max-width:1400px){
 .account-info-card{
-    height:20rem;
+    height:max-content;
+    justify-content: space-evenly;
 }
 }
 @media(max-width:824px){
     .account-info-card{
-        height: 40rem;
+        height: max-content;
     }
 }
 </style>
