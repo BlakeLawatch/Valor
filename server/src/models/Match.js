ebe18e25480fb6a80bf99e5ca0204b2f5a64d0e3
alaynadelauro <andelauro@yahoo.com>
2023-11-15 10:29:59 -0700

    put in notes on the match model in back end

--- server/src/models/Match.js
+++ server/src/models/Match.js
@@ -3,10 +3,13 @@ import { Schema } from "mongoose";
 export const matchSchema = new Schema(
   {
     player1Id: { type: Schema.Types.ObjectId },
+    // NOTE player 1 and 2 are set by put requests from the front end when people register for the tournament
     player2Id: { type: Schema.Types.ObjectId },
     roundNumber: { type: Number },
+    // NOTE When we draw these to the front end, they can be put into an array in the AppState for the current tournament. The round number can be their position in the array +1(put request)
     tournamentId: { type: Schema.Types.ObjectId, required: true },
     winnerId: { type: Schema.Types.ObjectId },
+    // NOTE this would be assigned in the manage tournament function by the winner of the tournament
   },
   {
     timestamps: true, toJSON: { virtuals: true }
