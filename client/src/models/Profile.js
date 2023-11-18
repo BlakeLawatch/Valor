e85beb6ee7ab79d2dfa16174ffbb9ecbf31f199b
alaynadelauro <andelauro@yahoo.com>
2023-11-17 17:35:31 -0700

    changed edit account modal

--- client/src/models/Profile.js
+++ client/src/models/Profile.js
@@ -4,7 +4,7 @@ export class Profile{
         this.name = data.name
         this.picture = data.picture
         this.bio = data.bio
-        this.coverImg = data.coverImg || 'https://images.unsplash.com/photo-1620870300436-a70b605f6ebe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGJhbWJvb3xlbnwwfHwwfHx8MA%3D%3D'
+        this.coverImg = data.coverImg || '/src/assets/img/valorPanda.png'
         this.twitchUrl = data.twitchUrl
         this.youtubeUrl = data.youtubeUrl
         this.twitterUrl = data.twitterUrl
